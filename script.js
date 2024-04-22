5a5e5f3c9664bbcfaba5cd65071e55e08a0fed4d
dhkiller350 <100392451+dhkiller350@users.noreply.github.com>
2024-04-21 21:38:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-alert("Please note this website and my other website if just examples any reports done on my instagram account will be taken action againt you(the viewer) please enjoy)
+alert("Please note this website and my other website if just examples any reports done on my instagram account will be taken action against you(the viewer) please enjoy")
